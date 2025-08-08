161b0601f664946d6534c999242a32b3ba2e7bb5
johnnyt16 <johntaylormcc@gmail.com>
2025-08-08 01:07:41 -0400

    import issue

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,7 +1,5 @@
 from .config import Config, get_alpaca_client, test_connection
 from .database import DatabaseManager, Trade, Position, Performance, Signal
-from .risk_manager import RiskManager
-from .optimal_schedule import OptimalTradingSchedule
 
 __all__ = [
     'Config',
@@ -11,7 +9,5 @@ __all__ = [
     'Trade',
     'Position',
     'Performance',
-    'Signal',
-    'RiskManager',
-    'OptimalTradingSchedule'
+    'Signal'
 ]
